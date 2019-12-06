ee9d332b50111268f248a6468f5abf173aa2f648
梁涛 <320603976@qq.com>
2019-12-06 09:18:29 +0800

    初建工具类

--- src/api/WebUtil.js
+++ src/api/WebUtil.js
@@ -3,6 +3,20 @@ var util = {
   // 目前使用没问题
   messageBox: function (msg) {
     MessageBox.alert(msg)
+  },
+  msgs: function () {
+    // 手机版和页面版的提示
+    this.$alert('会话过期，请重新登录', '提示', {
+      confirmButtonText: '确定',
+      showClose: false,
+      callback: action => {
+        // this.$message({
+        //   type: 'info',
+        //   message: `action: ${ action }`
+        // });
+        this.$router.push('/home')
+      }
+    })
   }
 }
 const WebUtil = {
