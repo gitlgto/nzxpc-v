c965dba95a1ff458f0acbedfc0bf3b3f070e2a31
梁涛 <320603976@qq.com>
2019-12-11 10:19:11 +0800

    修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -95,13 +95,14 @@ export default {
           //     this.$message.success('登陆成功')
           //   }
           // })
+          localStorage.setItem('token', 1111)
           this.$router.push('/home')
-          // 再次点击重新访问
+          // 再次点击重新访问 TODO 出现一种情况是先进home再设置token，会出现出现会话过期，点击后还是回到main，是因为连续走了两次判断
           // localStorage.setItem('token', 1111)
           // var that = this
           // that.goLogins(false)
           // console.log(that.loginForm.username)
-          // this.$message.success('登陆成功')
+          this.$message.success('登陆成功')
           // window.sessionStorage.setItem('token', 98177010213298356078)
         }
       })
